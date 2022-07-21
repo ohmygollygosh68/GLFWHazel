12f1d56b6a89bc849747e1e9d38df83711563a77
ohmygollygosh68 <ohmygollygosh@outlook.com>
2022-07-21 16:42:40 +0100

    Add project files.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// For use by Hazel applications
+
+#include "Hazel/Application.h"
+#include "Hazel/Layer.h"
+#include "Hazel/Log.h"
+
+// ---Entry Point---------------------
+#include "Hazel/EntryPoint.h"
+// -----------
